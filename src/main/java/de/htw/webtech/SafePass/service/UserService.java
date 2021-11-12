31324572997119e555c577b882582ad4f4f99026
ilia <ilyasokolovskij@gmail.com>
2021-11-12 21:57:19 +0100

    Milestone 2 reached. Can delete a specific user from the DB [v0.2]

--- src/main/java/de/htw/webtech/SafePass/service/UserService.java
+++ src/main/java/de/htw/webtech/SafePass/service/UserService.java
@@ -4,10 +4,7 @@ import de.htw.webtech.SafePass.persistence.UserEntity;
 import de.htw.webtech.SafePass.persistence.UserRepository;
 import de.htw.webtech.SafePass.web.api.User;
 import de.htw.webtech.SafePass.web.api.UserManipulationRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -54,6 +51,15 @@ public class UserService {
         userRepository.save(userEntity);
 
         return transformEntity(userEntity);
+    }
+
+    public boolean deleteById(Long id) {
+        if (!userRepository.existsById(id)) {
+            return false;
+        }
+
+        userRepository.deleteById(id);
+        return true;
     }
 
     private User transformEntity(UserEntity userEntity) {
